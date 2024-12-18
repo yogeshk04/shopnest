42df322a4517158c8be926e04d9e9dda6ab77277
yogesh nikam <yogesh.nikam@siemens.com>
2024-12-18 16:44:45 +0100

    Gateway updates

--- shopnest-api/src/main/java/com/api/server/controllers/UserController.java
+++ shopnest-api/src/main/java/com/api/server/controllers/UserController.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
      @GetMapping("/user")
--- shopnest-gateway/src/main/java/com/auth/client/ShopnestGatewayApplication.java
+++ shopnest-gateway/src/main/java/com/auth/client/ShopnestGatewayApplication.java
@@ -2,6 +2,10 @@ package com.auth.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ShopnestGatewayApplication {
@@ -10,4 +14,8 @@ public class ShopnestGatewayApplication {
 		SpringApplication.run(ShopnestGatewayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver userKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
 }
